34260b7a6a6287076cb9ef0d30f30eccc35767a5
VP Keerthan <keerthanvp@gmail.com>
2021-06-19 13:44:48 +0530

    Initial Commit

---
+++ src/test/java/com/vpk/tutorial/springcloud/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpk.tutorial.springcloud.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
